0b9330fa288d3db3709d3fbe653d4ce17b3b4f25
IsabellaJB <isabellajib5@gmail.com>
2024-02-27 17:19:28 -0600

    Paquete

--- setup.py
+++ setup.py
@@ -6,20 +6,18 @@ LONG_DESCRIPTION = 'Mi primer paquete de Python con una descripción ligeramente
 
 # Configurando
 setup(
-    # el nombre debe coincidir con el nombre de la carpeta 	  
-    #'modulomuysimple'
-    name =" modulomuysimple", 
+    # el nombre debe coincidir con el nombre de la carpeta
+    # 'modulomuysimple'
+    name="modulomuysimple",  # Corregido: eliminado espacio en blanco extra
     version=VERSION,
     author="Isabella Jimenez Bravo",
-    author_email="<arcoiris_11cha@email.com>",
+    author_email="arcoiris_11cha@email.com",  # Corregido: sin < y >
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[], # añade cualquier paquete adicional que debe ser
-    #instalado junto con tu paquete. Ej: 'caer'
-        
+    install_requires=[],  # añade cualquier paquete adicional que debe ser instalado junto con tu paquete. Ej: 'caer'
     keywords=['python', 'primer paquete'],
-    classifiers= [
+    classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
         "Programming Language :: Python :: 2",
