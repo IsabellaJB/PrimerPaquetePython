653dd2e84da7863c941beafd121d5317bdd7719b
IsabellaJB <isabellajib5@gmail.com>
2024-03-01 20:19:50 -0600

    ......

--- build/lib/primer_paquete_ux_isa_2/__init__.py
+++
@@ -1,5 +0,0 @@
-from .suma import suma
-from .resta import resta
-
-
-# from extras import divide
--- build/lib/primer_paquete_ux_isa_2/extras/divide.py
+++
@@ -1,5 +0,0 @@
-def divide(a, b):
-    if b == 0:
-        print("Error: División por cero.")
-        return None
-    return a / b
