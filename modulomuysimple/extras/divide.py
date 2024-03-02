130de41f634c5bbf42c41157c16d5536930e5c69
IsabellaJB <isabellajib5@gmail.com>
2024-03-01 18:39:50 -0600

    paquete

---
+++ modulomuysimple/extras/divide.py
@@ -0,0 +1,9 @@
+def dividir(a, b):
+    """Divide dos números.
+    
+    Retorna None si el divisor es 0.
+    """
+    if b == 0:
+        print("Error: División por cero.")
+        return None
+    return a / b
